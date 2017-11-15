db4144bf211c888819fb654f65f03ac4e7875f3c
rodiw <rodi.watti@gmail.com>
2017-11-15 17:33:53 +0100

    the get and also post querys work. Moving on to delete and update

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,23 @@
 var url = 'https://timesheet-1172.appspot.com/42beb4d4/notes';
 
-var queryBtn = document.querySelector('#get-query');
+var getQueryBtn = document.querySelector('#get-query');
+var postQueryBtn = document.querySelector('#post-query');
 
 
-queryBtn.addEventListener('click', function () {
+getQueryBtn.addEventListener('click', function () {
     axios.get(url)
     .then(function (response) {
-        console.log(response);
+        console.log(response.data);
     });
+});
+
+postQueryBtn.addEventListener('click', function () {
+    axios({
+        method: 'POST',
+        url: url,
+        data: {
+            description: "Checking if this post-request works",
+            title: "Post-request"
+        }
+    }); 
 });

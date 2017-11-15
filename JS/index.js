b365d223bde5b7fdae6981527f98ad3601fb3917
rodiw <rodi.watti@gmail.com>
2017-11-15 17:17:14 +0100

    added a button to get the query

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,11 @@
 var url = 'https://timesheet-1172.appspot.com/42beb4d4/notes';
 
+var queryBtn = document.querySelector('#get-query');
+
+
+queryBtn.addEventListener('click', function () {
+    axios.get(url)
+    .then(function (response) {
+        console.log(response);
+    });
+});

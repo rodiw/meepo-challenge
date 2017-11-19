ab1a21c0e31f2d63e237d1d98c7d4ca28bf9f6e0
rodiw <rodi.watti@gmail.com>
2017-11-19 22:56:24 +0100

    All the functions works as intended. Changed the h2,h3 to inputs insted so you can edit on the spot. The refresh happens with the help of ajax. Next step, overline text in inputs when you press the done button

--- JS/index.js
+++ JS/index.js
@@ -7,15 +7,15 @@ var url = 'https://timesheet-1172.appspot.com/42beb4d4/notes';
 var getQueryBtn = document.querySelector('#get-query');
 var postQueryBtn = document.querySelector('#post-query');
 var putQueryBtn = document.querySelector('#put-query');
-var titleInput = document.querySelector("#title");
-var descriptionInput = document.querySelector("#description");
+var titleInput = document.querySelector(".title");
+var descriptionInput = document.querySelector(".description");
 
 
 
 /* GET, POST, PUT and DELETE logic */
-getQueryBtn.addEventListener('click', function () {
+/*getQueryBtn.addEventListener('click', function () {
     getQuerys();
-});
+});*/
 
 postQueryBtn.addEventListener('click', function () {
     axios({
@@ -26,28 +26,35 @@ postQueryBtn.addEventListener('click', function () {
             title: titleInput.value
         } 
     })
-    .then(setTimeout(getQuerys, 400))
+    .then(function (val) {
+        $('.list-holder').append("<div id=" + val.id + " class='list-card'><i data-id=" + val.id + " class='fa fa-check fa-2x done'></i><div data-id=" + val.id + "><input type = 'text' class = 'title' placeholder = 'Title' value='" + val.title + "'/><input value='" + val.description + "' type = 'text' class = 'description' placeholder = 'Description'/></div><i data-id=" + val.id + " class='fa fa-pencil-square-o fa-2x edit'></i><i data-id=" + val.id + " class='fa fa-times fa-2x delete'></i></div>");
+    })
     .catch(function (error) {
         console.log(error);
     });
 });
 
-/*putQueryBtn.addEventListener('click', function () {
-    axios.put(url + '/33', {
+$('.list-holder').on('click', '.edit', function () {
+    let id = $(this).attr("data-id");
+    axios.put(url + '/' + id, {
         description: descriptionInput.value,
         title: titleInput.value
     })
-    .then(getQuerys)
+    .then(function (val) {
+        $('div[id=' + id + ']').remove();
+        $('.list-holder').append("<div id=" + val.id + " class='list-card'><i data-id=" + val.id + " class='fa fa-check fa-2x done'></i><div data-id=" + val.id + "><input type = 'text' class = 'title' placeholder = 'Title' value='" + val.title + "'/><input value='" + val.description + "' type = 'text' class = 'description' placeholder = 'Description'/></div><i data-id=" + val.id + " class='fa fa-pencil-square-o fa-2x edit'></i><i data-id=" + val.id + " class='fa fa-times fa-2x delete'></i></div>");
+    })
     .catch(function (error) {
         console.log(error);
     });
-
-
-});*/
+});
 
 $('.list-holder').on('click', '.delete', function () {
-    axios.delete(url + '/' + $(this).attr("data-id")) // Gets data-id attribute and deletes it
-    .then(setTimeout(getQuerys, 400))
+    let id = $(this).attr("data-id");
+    axios.delete(url + '/' + id) // Gets data-id attribute and deletes it
+    .then(function () {
+        $('div[id=' + id + ']').remove();
+    })
     .catch(function (error) {
         console.log(error);
     });
@@ -60,7 +67,9 @@ $('.list-holder').on('click', '.done', function () {
 function printMethod (arr) {
     $('.list-holder').empty()
     arr.forEach(function (val) {
-        $('.list-holder').append("<div class='list-card'><i data-id=" + val.id + " class='fa fa-check fa-2x done'></i><div data-id=" + val.id + "><h2>" + val.title + "</h2><h3>" + val.description + "</h3></div><i data-id=" + val.id + " class='fa fa-pencil-square-o fa-2x edit'></i><i data-id=" + val.id + " class='fa fa-times fa-2x delete'></i></div>");
+        let title = val.title;
+        console.log(title);
+        $('.list-holder').append("<div id=" + val.id + " class='list-card'><i data-id=" + val.id + " class='fa fa-check fa-2x done'></i><div data-id=" + val.id + "><input type = 'text' class = 'title' placeholder = 'Title' value='" + val.title + "'/><input value='" + val.description + "' type = 'text' class = 'description' placeholder = 'Description'/></div><i data-id=" + val.id + " class='fa fa-pencil-square-o fa-2x edit'></i><i data-id=" + val.id + " class='fa fa-times fa-2x delete'></i></div>");
     });
 }
 

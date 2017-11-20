68f1040579e1e67232fbdf30c71f8a7d81f17a0e
rodiw <rodi.watti@gmail.com>
2017-11-20 16:32:25 +0100

    removed some redundant code in JS, HTML and CSS. Next step, better UI flow and also remove more redundancy

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,21 @@
 /* Get the queries */
 window.onload = function () {
-    getQuerys();
+    axios.get(url)
+    .then(function (response) {
+        response.data.forEach(function (val) {
+            $('.list-holder').append(`<div id="${val.id}" class="list-card"><i data-id="${val.id}" class="fa fa-check fa-2x done"></i><div data-id="${val.id}"><input data-id="${val.id}" type = "text" class = "title" placeholder = "Add an task" value="${val.title}"/><input data-id="${val.id}" value="${val.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${val.id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${val.id}" class="fa fa-times fa-2x delete"></i></div>`);
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 }
 /* Variables declaration */
 var url = 'https://timesheet-1172.appspot.com/42beb4d4/notes';
-var getQueryBtn = document.querySelector('#get-query');
 var postQueryBtn = document.querySelector('#post-query');
-var putQueryBtn = document.querySelector('#put-query');
 var titleInput = document.querySelector(".add-title");
 var descriptionInput = document.querySelector(".add-description");
 
-
-
-/* GET, POST, PUT and DELETE logic */
-/*getQueryBtn.addEventListener('click', function () {
-    getQuerys();
-});*/
 
 postQueryBtn.addEventListener('click', function () {
     axios({
@@ -27,7 +27,7 @@ postQueryBtn.addEventListener('click', function () {
         } 
     })
     .then(function (val) {
-        $('.list-holder').append(`<div id="${val.data.id}" class="list-card"><i data-id="${val.data.id}" class="fa fa-check fa-2x done"></i><div data-id="${val.data.id}"><input data-id="${val.data.id}" type = "text" class = "title" placeholder = "Title" value="${val.data.title}"/><input data-id="${val.data.id}" value="${val.data.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${val.data.id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${val.data.id}" class="fa fa-times fa-2x delete"></i></div>`);
+        $('.list-holder').append(`<div id="${val.data.id}" class="list-card"><i data-id="${val.data.id}" class="fa fa-check fa-2x done"></i><div data-id="${val.data.id}"><input data-id="${val.data.id}" type = "text" class = "title" placeholder = "Add an task" value="${val.data.title}"/><input data-id="${val.data.id}" value="${val.data.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${val.data.id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${val.data.id}" class="fa fa-times fa-2x delete"></i></div>`);
     })
     .catch(function (error) {
         console.log(error);
@@ -38,14 +38,13 @@ $('.list-holder').on('click', '.edit', function () {
     let id = $(this).attr("data-id");
     let titleVal = $(`.title[data-id="${id}"]`).val();
     let descriptionVal = $(`.description[data-id="${id}"]`).val();
-    console.log(titleVal, descriptionVal);
     axios.put(url + '/' + id, {
         description: descriptionVal,
         title: titleVal
     })
     .then(function (val) {
-        $('div[id=' + id + ']').remove();
-        $('.list-holder').append(`<div id="${id}" class="list-card"><i data-id="${id}" class="fa fa-check fa-2x done"></i><div data-id="${id}"><input data-id="${id}" type = "text" class = "title" placeholder = "Title" value="${val.data.title}"/><input data-id="${id}" value="${val.data.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${id}" class="fa fa-times fa-2x delete"></i></div>`);
+        $('div[id=' + val.data.id + ']').remove();
+        $('.list-holder').append(`<div id="${val.data.id}" class="list-card"><i data-id="${val.data.id}" class="fa fa-check fa-2x done"></i><div data-id="${val.data.id}"><input data-id="${val.data.id}" type = "text" class = "title" placeholder = "Add an task" value="${val.data.title}"/><input data-id="${val.data.id}" value="${val.data.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${val.data.id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${val.data.id}" class="fa fa-times fa-2x delete"></i></div>`);
     })
     .catch(function (error) {
         console.log(error);
@@ -67,22 +66,5 @@ $('.list-holder').on('click', '.done', function () {
     $('input[data-id=' + $(this).attr('data-id') +']').toggleClass('finished');
 });
 
-function printMethod (arr) {
-    $('.list-holder').empty()
-    arr.forEach(function (val) {
-        let title = val.title;
-        console.log(title);
-        $('.list-holder').append(`<div id="${val.id}" class="list-card"><i data-id="${val.id}" class="fa fa-check fa-2x done"></i><div data-id="${val.id}"><input data-id="${val.id}" type = "text" class = "title" placeholder = "Title" value="${val.title}"/><input data-id="${val.id}" value="${val.description}" type = "text" class = "description" placeholder = "Description"/></div><i data-id="${val.id}" class="fa fa-pencil-square-o fa-2x edit"></i><i data-id="${val.id}" class="fa fa-times fa-2x delete"></i></div>`);
-    });
-}
 
-function getQuerys () {
-    axios.get(url)
-    .then(function (response) {
-        printMethod(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-}
 
